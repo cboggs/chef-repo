c198905abf3c7ade22596dde6e3a6dc50288792a
Cody Boggs <cboggs@rallydev.com>
2013-03-07 14:02:35 -0700

    various learnin's

--- roles/base.rb
+++ roles/base.rb
@@ -1,10 +1,12 @@
 name "base"
 description "Base Server Role"
-def_run_list = ["recipe[motd]", "recipe[users]"]
-env_run_lists({
-  "_default" => def_run_list,
-  "dev" => def_run_list,
-  "production" => [
-    "recipe[motd]"
-  ]
-})
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client]", "recipe[motd]", "recipe[users]"
+
+#def_run_list = ["recipe[motd]", "recipe[users]"]
+#env_run_lists({
+#  "_default" => def_run_list,
+#  "dev" => def_run_list,
+#  "production" => [
+#    "recipe[motd]"
+#  ]
+#})
